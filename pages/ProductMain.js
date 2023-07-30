1e22a7818f4cdf7f6a8defb416ad2a175ad5f78b
santhoshkumar032001 <129770099+santhoshkumar032001@users.noreply.github.com>
2023-07-30 16:47:04 +0530

    Add files via upload

---
+++ pages/ProductMain.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import ProductList from "./ProductList";
+import Footer from "./Footer";
+
+const Home = () => {
+  return (
+    <>
+        <Header></Header>
+        <ProductList></ProductList>
+        <Footer></Footer>
+    </>
+  );
+};
+
+export default Home;
